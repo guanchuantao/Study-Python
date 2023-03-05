f7ab06bee7ce527e650e07a2d48f82032ddc6b4b
Chuantao.Guan <guanchuantao6312@163.com>
2023-03-05 21:39:04 +0800

    代码初始定义

---
+++ KangYongqi/DataTypeK.py
@@ -0,0 +1,3 @@
+# @author :  Chuantao.Guan
+# @time : 2023-03-05
+# @describe : 这个文件是康永棋写python数据类型的地方
---
+++ WeiZihao/DataTypeW.py
@@ -0,0 +1,3 @@
+# @author :  Chuantao.Guan
+# @time : 2023-03-05
+# @describe : 这个文件是魏子豪写python数据类型的地方
