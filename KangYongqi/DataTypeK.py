de4b20a2c04e399a9c726c1428bd31a207e17ec6
KYQNIUB <3160859991@qq.com>
2023-03-07 10:03:48 +0800

    代码初始定义

--- KangYongqi/DataTypeK.py
+++ KangYongqi/DataTypeK.py
@@ -17,39 +17,39 @@ c = True
 d = "hello,world! "
 
 # 列表类型list
-e = [6,7,8,'六']
+e = [6, 7, 8, '六']
 
 # 元组类型tuple
-f = (1,6,'liu',6.6)
+f = (1, 6, 'liu', 6.6)
 
 # 字典类型dict
-g = {"六":6,"liu":"六"}
+g = {"六": 6, "liu": "六"}
 
 # 类型之间的转换
 # 将整数类型转换为浮点类型
 h = float(a)
-print(h,type(h))
+print(h, type(h))
 
 # 将浮点类型转换为整数类型
 i = int(b)
-print(i,type(i))
+print(i, type(i))
 
 # 将字符串类型转换为整数类型
 j = int("666666")
-print(j,type(j))
+print(j, type(j))
 
 # 将整数类型转换为字符串类型
 k = str(88888888)
-print(k,type(k))
+print(k, type(k))
 
 # 将列表类型转换为元组类型
 l = tuple(e)
-print(l,type(l))
+print(l, type(l))
 
 # 将元组类型转换为列表类型
 m = list(f)
-print(m,type(m))
+print(m, type(m))
 
 # 将字典类型转换为列表类型
 n = list(g)
-print(n,type(n))
+print(n, type(n))
